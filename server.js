2783c5069cc6c6e66de48e377e58cc14bbd8cff3
Sarvajit Gupta <91364026+sarvajit2002@users.noreply.github.com>
2023-08-13 12:02:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(cors())
 app.use('/api/v1/user',require("./routes/userRoutes"))
 app.use('/api/v1/admin',require("./routes/adminRoutes"))
 app.use('/api/v1/doctor',require("./routes/doctorRoutes"))
-app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'client','build')))
 app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+    res.sendFile(path.join(__dirname,'client','build','index.html'))
 })
 dotenv.config();
 
